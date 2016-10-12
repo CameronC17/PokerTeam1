027f57d536b6150da4ffc563875bd3a55dd63055
Laura Salivonaite <laura.salivonaite@skybettingandgaming.com>
2016-10-12 11:35:09 +0100

    bla

---
+++ PokerGameFE/app.js
@@ -0,0 +1,26 @@
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3001;
+
+app.use(express.static(__dirname + '/public'));
+
+
+// set the view engine
+app.set("view engine" , "jsx");
+
+app.use("/" , function(req, res){
+
+	res.render("index" , {api: process.env.API_URL || "192.10.10.150"});
+
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(port, function() {
+  console.log('> Server up and listening on port ' + port);
+});
+
+module.exports = app;
